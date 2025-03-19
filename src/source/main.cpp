b971b568536afded24f8c0f8777629df29924272
Bradley Marranca <account+githubcom@bradmarranca.com>
2025-03-19 19:45:52 -0400

    Update CMake and add game initialization in Silicon
    - Modified `CMakeLists.txt` to include header directories, link with the Silicon library, and set project properties for Windows executable and C++ standards.
    - Updated commit hash in the Silicon subproject.
    - Added `Silicon::init()` method to set the game name and resolution in `main.cpp`, and adjusted the `Silicon::gameLoop()` method signature.

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -1,7 +1,12 @@
 #include "Silicon.hpp"
 
+void Silicon::init()
+{
+	Silicon::setGameName("Grum");
+	Silicon::setGameResolution(Silicon::int2(800, 600));
+}
 
-
-void Silicon::gameLoop() {
+void Silicon::gameLoop()
+{
 	
 }
